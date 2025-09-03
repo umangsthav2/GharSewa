53571e881ffb3350e35d4bd474c9c2d5b1892fc1
unknown <mangaleeshrestha@gmail.com>
2025-09-03 22:30:57 +0545

    Implemented dummy version for rendering API data

--- gharsewa/lib/main.dart
+++ gharsewa/lib/main.dart
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body:LoginScreen()),
+      home: Scaffold(body:TestScreen()),
     );
   }
 }
--- gharsewa/lib/screens/TestScreen.dart
+++ gharsewa/lib/screens/TestScreen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import 'package:gharsewa/widgets/GSAppBar.dart';
-import 'package:gharsewa/widgets/GSButton.dart';
-import 'package:gharsewa/widgets/GSListTile.dart';
-import 'package:gharsewa/widgets/GSTextField.dart';
-import 'package:gharsewa/widgets/GSCard.dart';
-import 'package:gharsewa/widgets/GSUser.dart';
 
-class Testscreen extends StatefulWidget {
-  const Testscreen({super.key});
+// import 'package:gharsewa/widgets/GSAppBar.dart';
+// import 'package:gharsewa/widgets/GSButton.dart';
+// import 'package:gharsewa/widgets/GSListTile.dart';
+// import 'package:gharsewa/widgets/GSTextField.dart';
+// import 'package:gharsewa/widgets/GSCard.dart';
+// import 'package:gharsewa/widgets/GSUser.dart';
+
+
+
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+
+
+
+
+
   @override
   State createState() {
     return TestState();
@@ -16,10 +28,39 @@ class Testscreen extends StatefulWidget {
 }
 
 class TestState extends State {
+  
+  List<Widget> dataList  = [];
+  
   @override
   Widget build(BuildContext bc) {
+
+  
+  
+
     return Scaffold(
-      body:null,
+      body:Column(
+        children: [
+          InkWell(
+            child:Text("Helloowwwwwwwwwwwwwww"),
+            onTap:() async {
+          
+                
+                final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+                final data = await http.get(url);
+                debugPrint(data.body);
+
+                final List<dynamic> parsedData = jsonDecode(data.body);
+                
+                setState(() {
+                  dataList = parsedData.map((item) =>Text(item['title'])).toList();
+                });
+            },
+          ),
+        Column(
+          children: dataList,
+        ),
+        ],
+      ),
     );
   }
 }
